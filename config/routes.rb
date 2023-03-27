77126f54eeab7b8928f66f2e361522bbb4c43fd0
Marine Coltel <marine_coltel@hotmail.fr>
2023-03-27 06:35:23 +0200

    homepage decommentee
    Signed-off-by: Marine Coltel <marine_coltel@hotmail.fr>

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  # root to: "pages#home"
+  root to: "pages#home"
   resources :tournaments do
     resources :teams, only: %i[show new create]
   end
