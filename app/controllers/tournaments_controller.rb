eabd016cf9cb57cc371a16abe5a2f5029910d65a
gael carayon <gaelcarayon@hotmail.com>
2023-03-28 23:10:34 +0200

    correction de l'enregistrement de la participation aux tournois
    Signed-off-by: gael carayon <gaelcarayon@hotmail.com>

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -3,10 +3,10 @@ class TournamentsController < ApplicationController
 
   def join
     @tournament = Tournament.find(params[:id])
-    if current_user.user_tournaments.include?(@tournament)
+    if current_user.tournaments.include?(@tournament)
       flash[:alert] = "Vous êtes déjà inscrit à ce tournoi"
     else
-      @tournament.users << current_user
+      UserTournament.create(tournament: @tournament, user: current_user)
       flash[:notice] = "Vous êtes bien inscrit à ce tournoi"
     end
     redirect_to tournament_path(@tournament)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :tournaments, through: :user_tournaments
   has_many :user_tournaments, dependent: :destroy
+  has_many :tournaments, through: :user_tournaments
   has_many :teams, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
