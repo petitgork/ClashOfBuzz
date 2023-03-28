9a70da3280598cc3ff62ce395d57f3b8bf46bc13
bechara francis <becharafrancis@gmail.com>
2023-03-28 14:22:36 +0200

    teams
    Signed-off-by: bechara francis <becharafrancis@gmail.com>

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,2 +1,18 @@
 class TeamsController < ApplicationController
+  def new
+    @team = Team.new
+    @user = current_user
+  end
+  def create
+    @team = Team.new(params[:team])
+    @team.user_id = current_user.id
+    if @team.save
+      flash[:notice] = "Team created successfully."
+      redirect_to _path
+    else
+      render ender :new, status: :unprocessable_entity
+    end
+  end
+end
+
 end
