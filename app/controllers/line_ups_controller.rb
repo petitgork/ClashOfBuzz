cc86e1066be8e7fda6181a37e982fe28e6ba6c0d
bechara francis <becharafrancis@gmail.com>
2023-03-29 17:14:43 +0200

    yessss baby
    Signed-off-by: bechara francis <becharafrancis@gmail.com>

--- app/controllers/line_ups_controller.rb
+++ app/controllers/line_ups_controller.rb
@@ -1,43 +1,14 @@
 class LineUpsController < ApplicationController
-  def new
-    @line_up = LineUp.new
-    @user = current_user
-    @tournament = Tournament.find(params[:tournament_id])
-  end
-
   def create
     # création de l'équipe
-    line_up = LineUp.new(line_up_params)
-    line_up.user = current_user
-    tournament = Tournament.find(params[:tournament_id])
-    line_up.tournament = tournament
-
-    # selection des joueurs
-    @line_up = LineUp.where[:user, :politics].first
-    line_up.each do
-      line = Politic.find(line_up.politic_id)
-      line_up.politics << line
+    line_up = LineUp.new
+    line_up.match = Match.find(params[:match_id])
+    line_up.politic = Politic.find(params[:politic_id])
+    if line_up.save
+      flash[:notice] = "Votre équipe est prête."
+      redirect_to match_path(line_up.match)
+    else
+      render :new, status: :unprocessable_entity
     end
-    # condition de choix d'un joueur
-
-
-
-
-
-
-
-      if line_up.politics.count == 5 && line_up.save
-        flash[:notice] = "Votre équipe est prête."
-        redirect_to user_path(current_user)
-      else
-        render ender :new, status: :unprocessable_entity
-      end
-    end
-  end
-
-  private
-
-  def line_up_params
-    params.require(:line_up).permit(:politic_id, :match_id)
   end
 end
