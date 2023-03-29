f5473573b98e7fa414d2db080494dbfe1aadfa17
gael carayon <gaelcarayon@hotmail.com>
2023-03-29 11:39:00 +0200

    quand je rejoins le tournoi : envoi sur creation de team
    Signed-off-by: gael carayon <gaelcarayon@hotmail.com>

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -9,7 +9,7 @@ class TournamentsController < ApplicationController
       UserTournament.create(tournament: @tournament, user: current_user)
       flash[:notice] = "Vous êtes bien inscrit à ce tournoi"
     end
-    redirect_to tournament_path(@tournament)
+    redirect_to new_tournament_team_path(@tournament)
   end
 
   def index
