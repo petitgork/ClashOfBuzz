11fe3817cbcfe760cb2777f267690ac15ee222d9
HervÃe Nguetsop <herve.nguetsop@gmail.com>
2023-03-31 12:59:49 +0200

    match_result to winner
    Signed-off-by: HervÃe Nguetsop <herve.nguetsop@gmail.com>

--- db/migrate/20230331102927_rename_match_result_to_winner_in_matches.rb
+++ db/migrate/20230331102927_rename_match_result_to_winner_in_matches.rb
@@ -1,5 +1,6 @@
 class RenameMatchResultToWinnerInMatches < ActiveRecord::Migration[7.0]
   def change
     rename_column :matches, :match_result, :winner
+    change_column :matches, :winner, :string
   end
 end
