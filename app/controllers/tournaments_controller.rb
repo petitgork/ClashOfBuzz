3bfd0ce41289a93cb37de88ac1ec5ff7a04dfe12
Marine Coltel <marine_coltel@hotmail.fr>
2023-04-03 19:59:58 +0200

    carousel fix
    Signed-off-by: Marine Coltel <marine_coltel@hotmail.fr>

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -36,10 +36,12 @@ class TournamentsController < ApplicationController
   def show
     @team = Team.where(user: current_user, tournament: @tournament).first
     @matches = @tournament.matches
-    @politics_slice_a = @team.politics.slice(0, 4)
-    @politics_slice_b = @team.politics.slice(4, 4)
-    @politics_slice_c = @team.politics.slice(8, 4)
-    @politics_slice_d = @team.politics.slice(12, 4)
+    if @team.politics.count > 0
+      @politics_slice_a = @team.politics.slice(0, 4)
+      @politics_slice_b = @team.politics.slice(4, 4)
+      @politics_slice_c = @team.politics.slice(8, 4)
+      @politics_slice_d = @team.politics.slice(12, 4)
+    end
     @tournament = Tournament.find(params[:id])
     tournament_results(@tournament)
 
