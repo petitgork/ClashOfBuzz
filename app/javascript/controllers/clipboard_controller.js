0b0dd37904fbc13188deaa3cc5f796f814fde69b
Marine Coltel <marine_coltel@hotmail.fr>
2023-04-03 16:06:38 +0200

    carousel en cours
    Signed-off-by: Marine Coltel <marine_coltel@hotmail.fr>

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -1,12 +1,14 @@
 const copyButton = document.querySelector('#copy-button');
 const input = document.querySelector('input');
 
-copyButton.addEventListener('click', () => {
-  navigator.clipboard.writeText(input.value)
-    .then(() => {
-      copyButton.innerText = "Copié !";
-    })
-    .catch((error) => {
-      console.error('Failed to copy text: ', error);
-    });
-});
+if (copyButton) {
+  copyButton.addEventListener('click', () => {
+    navigator.clipboard.writeText(input.value)
+      .then(() => {
+        copyButton.innerText = "Copié !";
+      })
+      .catch((error) => {
+        console.error('Failed to copy text: ', error);
+      });
+  });
+}
