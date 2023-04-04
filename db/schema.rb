2e0222268bf587aa50c96494ce6ababf07424447
bechara francis <becharafrancis@gmail.com>
2023-04-04 14:39:27 +0200

    404 3
    Signed-off-by: bechara francis <becharafrancis@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_04_075903) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_04_105402) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -71,6 +71,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_04_075903) do
     t.string "avatar"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "punch_line"
   end
 
   create_table "team_matches", force: :cascade do |t|
